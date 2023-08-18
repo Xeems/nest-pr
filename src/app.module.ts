92192ab2cddb92c1ae27a72e1920d2a8bf4fbd53
roman krasilnikov <krasilnikovroman5@gmail.com>
2023-08-18 17:22:03 +0700

    graphql initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseService } from './database/database.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { PrismaService } from './prisma/prisma.service';
+import { join } from 'path';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    UserModule
+  ],
   controllers: [AppController],
-  providers: [AppService, DatabaseService],
+  providers: [AppService, PrismaService],
 })
-export class AppModule {}
+export class AppModule { }
